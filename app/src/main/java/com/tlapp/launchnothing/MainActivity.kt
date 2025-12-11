b89509a94ce878d51d850c845c38c3222df60ffc
Trym Lekva <tarjeison@gmail.com>
2025-12-11 18:41:03 +0100

    Use correct hilt import

--- app/src/main/java/com/tlapp/launchnothing/MainActivity.kt
+++ app/src/main/java/com/tlapp/launchnothing/MainActivity.kt
@@ -15,7 +15,7 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
-import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
 import com.tlapp.launchnothing.data.models.AppInfo
 import com.tlapp.launchnothing.ui.theme.AppTheme
 import com.tlapp.launchnothing.ui.theme.LaunchNothingTheme
