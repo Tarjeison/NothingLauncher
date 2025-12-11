09d68aee055666c85bf3c9dbfd18f15f117ef2bc
Trym Lekva <tarjeison@gmail.com>
2025-12-11 18:46:54 +0100

    Enable edge to edge

--- app/src/main/java/com/tlapp/launchnothing/MainActivity.kt
+++ app/src/main/java/com/tlapp/launchnothing/MainActivity.kt
@@ -3,12 +3,14 @@ package com.tlapp.launchnothing
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
@@ -25,24 +27,30 @@ import dagger.hilt.android.AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
         setContent {
             LaunchNothingTheme {
-                AppLauncher()
+                Scaffold { innerPadding ->
+                    AppLauncher(
+                        modifier = Modifier
+                            .padding(innerPadding)
+                    )
+                }
             }
         }
     }
 }
 
 @Composable
-fun AppLauncher(viewModel: AllAppsViewModel = hiltViewModel()) {
+fun AppLauncher(modifier: Modifier = Modifier, viewModel: AllAppsViewModel = hiltViewModel()) {
     val apps by viewModel.apps.collectAsState()
-    AppList(apps = apps)
+    AppList(apps = apps, modifier = modifier)
 }
 
 @Composable
-fun AppList(apps: List<AppInfo>) {
+fun AppList(apps: List<AppInfo>, modifier: Modifier = Modifier) {
     val context = LocalContext.current
-    LazyColumn(modifier = Modifier.fillMaxSize()) {
+    LazyColumn(modifier = modifier.fillMaxSize()) {
         items(apps) { app ->
             Text(
                 text = app.label,
